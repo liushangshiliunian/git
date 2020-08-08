ffc645cc45a0e304efb631b5dd5f461b9b85a6e1
兰宇航 <934152372@qq.com>
2020-08-08 22:14:01 +0800

    吸星第2次

--- src/com/atguigu/wugong/Xixingdafa.java
+++ src/com/atguigu/wugong/Xixingdafa.java
@@ -3,5 +3,7 @@ package com.atguigu.wugong;
 public class Xixingdafa {
     public static void main(String[] args) {
         System.out.println("吸取十年功力");
+        System.out.println("吸取二十年功力");
+        System.out.println("吸取三十年功力");
     }
 }

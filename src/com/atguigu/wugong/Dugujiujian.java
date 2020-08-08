9e154d181ba0b77716479e798596c020dbc549da
兰宇航 <934152372@qq.com>
2020-08-08 22:10:54 +0800

    第六次

--- src/com/atguigu/wugong/Dugujiujian.java
+++ src/com/atguigu/wugong/Dugujiujian.java
@@ -5,8 +5,8 @@ import com.sun.xml.internal.ws.api.model.wsdl.WSDLOutput;
 public class Dugujiujian {
     public static void main(String[] args) {
         System.out.println("fsdfdsf");
-    public static void main(String[] args)
-    {
+
+
         System.out.println("荡剑式");
         System.out.println("浪剑式");
         System.out.println("平剑式");
